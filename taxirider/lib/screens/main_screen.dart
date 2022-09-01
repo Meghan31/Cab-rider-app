4d30478bf791daf9633b475a976387b3c76061f7
Meghan31 <pulakhandammegha@gmail.com>
2022-09-01 22:09:59 +0530

    🚑 google maps with live location is now available

--- taxirider/lib/screens/main_screen.dart
+++ taxirider/lib/screens/main_screen.dart
@@ -1,10 +1,11 @@
-import 'dart:async';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:async';
+import 'package:geocoding/geocoding.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:taxirider/screens/login_screen.dart';
+import 'package:taxirider/widgets/divider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -14,40 +15,382 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  String location = 'Null, Press Button';
+  String address = 'search';
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   late GoogleMapController newGoogleMapController;
+
+  GlobalKey<ScaffoldState> scaffolfkey = GlobalKey<ScaffoldState>();
+
+  late Position currentPosition;
+  var geoLocator = Geolocator();
+  double bottomPaddingOfMap = 0;
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void locatePosition() async {
+    Position position = await _getGeoLocationPosition();
+    currentPosition = position;
+    LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latLngPosition, zoom: 14);
+    newGoogleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  // Future locatePosition() async {
+  //   Position position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.high);
+  //   currentPosition = position;
+
+  //   LatLng latlanPosition = LatLng(position.latitude, position.longitude);
+
+  //   CameraPosition cameraPosition =
+  //       CameraPosition(target: latlanPosition, zoom: 14);
+
+  //   newGoogleMapController
+  //       .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  // }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  // static final CameraPosition _kLake = CameraPosition(
+  //     bearing: 192.8334901395799,
+  //     target: LatLng(37.43296265331129, -122.08832357078792),
+  //     tilt: 59.440717697143555,
+  //     zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: scaffolfkey,
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
           title: const Text(
             'Main Screen',
             style: TextStyle(fontFamily: 'Brand-Bold'),
           ),
         ),
+        drawer: Container(
+          color: Colors.white,
+          width: 255,
+          child: Drawer(
+            child: ListView(
+              children: [
+                Container(
+                  height: 165,
+                  child: DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/user_icon.png",
+                            height: 65,
+                            width: 65,
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Profile Name",
+                                style: TextStyle(
+                                    fontFamily: "Brand-Bold", fontSize: 16),
+                              ),
+                              // SizedBox(
+                              //   height: 6,
+                              // ),
+                              // Text("Visit Profile")
+                            ],
+                          ),
+                        ],
+                      )),
+                ),
+                DividerWidget(),
+                SizedBox(
+                  height: 12,
+                ),
+                ListTile(
+                  leading: Icon(Icons.history),
+                  title: Text(
+                    "History",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(
+                    "Visit Profile",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text(
+                    "About",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
         body: Stack(
           children: [
             GoogleMap(
-              initialCameraPosition: _kGooglePlex,
+              padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
               mapType: MapType.normal,
-              myLocationButtonEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+              myLocationEnabled: true,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: true,
               onMapCreated: (GoogleMapController controller) {
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
+                setState(() {
+                  bottomPaddingOfMap = 300;
+                });
+                locatePosition();
               },
-            )
+            ),
+            // Positioned(
+            //   top: 45,
+            //   left: 325,
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       locatePosition();
+            //     },
+            //     child: const Icon(
+            //       Icons.my_location_outlined,
+            //       size: 40,
+            //       color: Colors.white70,
+            //       shadows: [
+            //         Shadow(
+            //           blurRadius: 10.0,
+            //           color: Colors.black54,
+            //           offset: Offset(5.0, 5.0),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            //Hamburger button for drawer
+            Positioned(
+              top: 45,
+              left: 22,
+              child: GestureDetector(
+                onTap: () {
+                  scaffolfkey.currentState?.openDrawer();
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(22),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 6,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7)),
+                      ]),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.black,
+                    ),
+                    radius: 20,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18),
+                    topRight: Radius.circular(18),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 16,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      const Text(
+                        'Hey there!!',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      const Text(
+                        'Where to!!',
+                        style:
+                            TextStyle(fontSize: 20, fontFamily: "Brand-Bold"),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 6,
+                              spreadRadius: 0.5,
+                              offset: Offset(0.7, 0.7),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.search,
+                                color: Colors.blueAccent,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text('Search Drop Off'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Add Home',
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text(
+                                'Your living home adderess',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    // fontFamily: "Brand-Bold",
+                                    color: Colors.grey[200]),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      DividerWidget(),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Add Home',
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text(
+                                'Your living home adderess',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    // fontFamily: "Brand-Bold",
+                                    color: Colors.grey[200]),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         )
         //
