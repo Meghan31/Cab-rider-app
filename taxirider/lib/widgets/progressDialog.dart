75f1e501bb2aacc0bd077bc89fcb11d90865bc39
Meghan31 <pulakhandammegha@gmail.com>
2022-09-01 22:10:50 +0530

    ✨ Shortcut for widgets

--- taxirider/lib/widgets/progressDialog.dart
+++ taxirider/lib/widgets/progressDialog.dart
@@ -12,8 +12,30 @@ class ProgressDialog extends StatelessWidget {
           width: 200,
           height: 200,
           decoration: BoxDecoration(
-            image: DecorationImage(
+            image: const DecorationImage(
               image: AssetImage('assets/gif/login_page.gif'),
+              fit: BoxFit.fill,
+            ),
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ));
+  }
+}
+
+class ProgressDialog2 extends StatelessWidget {
+  ProgressDialog2();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        child: Container(
+          // margin: EdgeInsets.all(15),
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/gif/signup_page.gif'),
               fit: BoxFit.fill,
             ),
             borderRadius: BorderRadius.circular(100),
